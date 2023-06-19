900efc9ae876a15043a1aded66a317e0adc663c8
taskeen999 <130865922+taskeen999@users.noreply.github.com>
2023-06-19 11:18:00 -0700

    Add project files.

---
+++ webSracpingApi/Controllers/ScrapperController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HtmlAgilityPack;
+using System.Net.Http;
+
+namespace webSracpingApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScrapperController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        public ScrapperController(ILogger<ScrapperController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("webscrape")]
+        public async Task<List<string>> webscrape( string website) {
+
+         List<string> result = new List<string>();
+            var web =  new HtmlWeb();
+            var htmldoc = web.Load(website);
+            var nodeElement = htmldoc.DocumentNode.SelectNodes("//div[@class='rankings-block__container full rankings-table']/table");
+
+            
+            foreach (var node in nodeElement)
+            {
+
+                result .Add( node.InnerHtml.FirstOrDefault().ToString());
+              // node.FirstChild.Element().F
+                    
+
+            }
+
+
+            
+
+
+            return result;
+        }
+        
+    }
+}
