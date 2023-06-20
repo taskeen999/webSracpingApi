0186dd279c1bab3a0f75c2d9e60d3843f242dd14
taskeen999 <130865922+taskeen999@users.noreply.github.com>
2023-06-20 12:46:39 -0700

    done

--- webSracpingApi/Controllers/ScrapperController.cs
+++ webSracpingApi/Controllers/ScrapperController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using HtmlAgilityPack;
 using System.Net.Http;
+using System.Xml.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace webSracpingApi.Controllers
 {
@@ -15,43 +17,60 @@ namespace webSracpingApi.Controllers
             _logger = logger;
         }
 
+
+
+        //  rankings-block__banner--pos
+        //rankings-block__banner--pos
+
+
         [HttpPost("TeamsRanking")]
         public async Task<IActionResult> TeamsRanking(enFormat format)
         {
             List<TeamStatus> teamsStatus = new List<TeamStatus>();
 
-            string website = $"https://www.icc-cricket.com/rankings/mens/team-rankings/${format.ToString()}";
+            string website = $"https://www.icc-cricket.com/rankings/mens/team-rankings/{format.ToString()}";
             var web = new HtmlWeb();
 
             var htmldoc = web.Load(website);
             var nodeElement = htmldoc.DocumentNode.SelectNodes("//div[@class='rankings-block__container full rankings-table']/table/tbody" +
                 "//tr");
+            var nodeindia = htmldoc.DocumentNode.SelectNodes("//div[@class='rankings-block__container full rankings-table']/table/tbody" +
+              "//tr[@class='rankings-block__banner']");
+
+            //u-show-phablet
+            HtmlDocument html = new HtmlDocument();
+            
 
             int iter = 0;
             foreach (var node in nodeElement)
             {
-                if (iter != 0)
-                {
+                //if (iter != 0)
+              //  {
                     var tds = node.InnerHtml;
                     HtmlDocument doc = new HtmlDocument();
                     doc.LoadHtml(tds);
                     var t = doc.DocumentNode.SelectNodes("//td");
 
-                    doc.LoadHtml(t[1].InnerHtml);
-                    var spans = doc.DocumentNode.SelectNodes("//span");
+                    //doc.LoadHtml(t[1].InnerHtml);
+                    // var spans = doc.DocumentNode.SelectNodes("//span");
+                    // var spans = doc.DocumentNode.SelectNodes("//span");
 
                     teamsStatus.Add(new TeamStatus
                     {
                         Position = t[0].InnerText.Trim(),
-                        Name = spans[1].InnerText,
+                        Name = t[1].InnerText.Trim().Replace("\n","").Remove(15).Trim(),
+                        // Name = t[1].InnerText.Substring(0,0),
+                      //  Name = t[1].InnerText.Remove(10),
                         Matches = t[2].InnerText.Trim(),
                         Points = t[3].InnerText.Trim(),
                         Rating = t[4].InnerText.Trim()
+                        
+                        
                     });
-                }
-                if (iter > 8)
-                    break;
-                iter++;
+          //      }
+               // if (iter > 8)
+                    //break;
+                //iter++;
             }
 
             return Ok(teamsStatus);
@@ -62,6 +81,7 @@ namespace webSracpingApi.Controllers
     public class TeamStatus
     {
         public string Position { get; set; }
+       // [MaxLength(7)]
         public string Name { get; set; }
         public string Matches { get; set; }
         public string Points { get; set; }
